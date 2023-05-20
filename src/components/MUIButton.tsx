995b2d9cfd820fa2fc3bcf00be6c6390f81b60cb
Ramshad <cpramshad@gmail.com>
2023-05-20 15:35:36 +0400

    Add icons and IconButton component

--- src/components/MUIButton.tsx
+++ src/components/MUIButton.tsx
@@ -1,4 +1,5 @@
-import { Button, Stack } from "@mui/material";
+import { Button, IconButton, Stack } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 export const MUIButton = () => {
   return (
@@ -38,6 +39,24 @@ export const MUIButton = () => {
         <Button variant="contained" size="large">
           large
         </Button>
+      </Stack>
+      <Stack direction="row" spacing={3}>
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => {
+            alert("Clicked");
+          }}
+        >
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+        <IconButton aria-label="send" color="success" size="small">
+          <SendIcon />
+        </IconButton>
       </Stack>
     </Stack>
   );
